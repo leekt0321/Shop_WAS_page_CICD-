01753239dc8e5304a8ff2be928e469ee81325070
leekt0321 <leekt0321@naver.com>
2024-06-28 07:57:06 +0000

    deploy test server

--- scripts/clean_old_files.sh
+++ scripts/clean_old_files.sh
@@ -2,3 +2,4 @@
 rm -rf /usr/local/tomcat/webapps/ROOT/*
 rm -f /usr/local/tomcat/conf/context.xml
 rm -f /usr/local/tomcat/conf/server.xml
+rm -f /usr/local/tomcat/lib/mysql-connector-java-8.0.23.jar
